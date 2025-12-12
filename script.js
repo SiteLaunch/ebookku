61a5594e191824a09e355f2a906edb15fb38d208
SiteLaunch <gutstrader7@gmail.com>
2025-12-13 05:25:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const ebooks = [
   {
-    title: "Ebook Contoh 1",
-    cover: "images/cover1.jpg"
+    title: "Kuasa Bisnes Online",
+    cover: "images/ebook1.png"
   },
   {
     title: "Ebook Contoh 2",
-    cover: "images/cover2.png"
+    cover: "images/ebook2.png"
+  },
+  {
+    title: "Ebook Contoh 3",
+    cover: "images/ebook3.png"
   }
 ];
+
+const list = document.getElementById("ebook-list");
+
+ebooks.forEach(e => {
+  const card = document.createElement("div");
+  card.style.textAlign = "center";
+  card.style.marginBottom = "20px";
+
+  card.innerHTML = `
+    <img src="${e.cover}" style="width:220px;border-radius:14px">
+    <p>${e.title}</p>
+  `;
+
+  list.appendChild(card);
+});
